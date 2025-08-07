21bca4484aea426e5f011867d857766fb12a489c
Diego Espinosa <espinosa9mx@gmail.com>
2025-08-07 13:12:22 -0600

    Hide scrollbars in Calendly modal - Add CSS rules and overflow-hidden to remove scrollbars from iframe

--- components/calendly-modal.tsx
+++ components/calendly-modal.tsx
@@ -21,7 +21,7 @@ export default function CalendlyModal({ isOpen, onClose }: CalendlyModalProps) {
       />
       
       {/* Modal */}
-      <div className="relative bg-white rounded-lg shadow-xl w-full max-w-4xl h-[80vh] mx-4">
+      <div className="relative bg-white rounded-lg shadow-xl w-full max-w-4xl h-[80vh] mx-4 overflow-hidden">
         {/* Header */}
         <div className="flex items-center justify-between p-4 border-b border-gray-200">
           <h3 className="text-lg font-medium text-gray-900">Schedule a Consultation</h3>
@@ -36,14 +36,18 @@ export default function CalendlyModal({ isOpen, onClose }: CalendlyModalProps) {
         </div>
         
         {/* Calendly iframe */}
-        <div className="flex-1 h-full">
+        <div className="flex-1 h-full overflow-hidden">
           <iframe
             src="https://calendly.com/keyan-keyrealestatecapital/30min?month=2025-08"
             width="100%"
             height="100%"
             frameBorder="0"
             title="Schedule a consultation"
-            className="rounded-b-lg"
+            className="rounded-b-lg calendly-iframe"
+            style={{ 
+              scrollbarWidth: 'none', 
+              msOverflowStyle: 'none'
+            }}
           />
         </div>
       </div>
